e314554a205a14649b99a6ad712c563111ea8c48
filip <filip>
2015-12-05 15:01:23 +0000

    azimuth calculation

--- app/src/main/java/com/magic_foo/magiconeapp/MainActivity.java
+++ app/src/main/java/com/magic_foo/magiconeapp/MainActivity.java
@@ -1,14 +1,93 @@
 package com.magic_foo.magiconeapp;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    TextView textView;
+    private final int TIMER_DELAY = 1000;
+    private SensorManager mSensorManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        textView = (TextView)findViewById(R.id.textView);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this);
+    }
+
+
+    private void initializeLoggingTimer() {
+        Thread timer = new Thread() {
+            public void run () {
+                for (;;) {
+                    // do stuff in a separate thread
+                    try {
+                        Thread.sleep(TIMER_DELAY);    // sleep for 5 min
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    timerEvent();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    protected void timerEvent() {
+/*
+        float[] mGravity;
+        float[] mGeomagnetic;
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null) {
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                azimut = orientation[0]; // orientation contains: azimut, pitch and roll
+            }
+        }*/
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get the angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+        textView.setText("Heading: " + Float.toString(degree) + " degrees");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
